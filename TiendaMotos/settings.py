6076848b751cf50a046fcb158c15b348277582c8
Manulynx <lynxiyo@gmail.com>
2026-03-14 00:19:25 -0400

    motosluxe.store

--- TiendaMotos/settings.py
+++ TiendaMotos/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-oow*)ra*=#g=akwmz&li%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = ['motosluxe.pythonanywhere.com',
+ALLOWED_HOSTS = ['www.motosluxe.store',
                  'tiendamotos-production.up.railway.app', 
                  'localhost', '127.0.0.1']
 CSRF_TRUSTED_ORIGINS = [
